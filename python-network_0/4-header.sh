d8c6aea1062515de1e647b63dabf730f25dd0d35
Dushime16 <f.dushime@alustudent.com>
2026-09-25 12:12:10 -0700

    Fix Task 4 header parameter format

--- python-network_0/4-header.sh
+++ python-network_0/4-header.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-# Sends a GET request to a URL with a custom header X-HolbertonSchool-User-Id: 98
+# Sends a GET request to a URL with a custom header
 curl -sH "X-HolbertonSchool-User-Id: 98" "$1"
