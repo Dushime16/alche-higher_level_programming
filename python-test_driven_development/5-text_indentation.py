96423d4c17b2f013bd6e1bbf61213b3a34df251e
Dushime16 <f.dushime@alustudent.com>
2026-09-25 18:57:31 -0700

    Add Task 4 text_indentation implementation and doctests

---
+++ python-test_driven_development/5-text_indentation.py
@@ -0,0 +1,32 @@
+#!/usr/bin/python3
+"""
+Module for text_indentation method.
+"""
+
+
+def text_indentation(text):
+    """
+    Prints a text with 2 new lines after each of these characters: '.', '?', and ':'
+
+    Args:
+        text (str): The text to format and print.
+
+    Raises:
+        TypeError: If text is not a string.
+    """
+    if not isinstance(text, str):
+        raise TypeError("text must be a string")
+
+    c = 0
+    while c < len(text) and text[c] == ' ':
+        c += 1
+
+    while c < len(text):
+        print(text[c], end="")
+        if text[c] in [".", "?", ":"]:
+            print("\n")
+            c += 1
+            while c < len(text) and text[c] == ' ':
+                c += 1
+            continue
+        c += 1
