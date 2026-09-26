272a231cdbb4fda929363ef982f462330526496a
Dushime16 <f.dushime@alustudent.com>
2026-09-25 21:25:55 -0700

    Implement Task 5: All cities by state

---
+++ python-object_relational_mapping/5-filter_cities.py
@@ -0,0 +1,36 @@
+#!/usr/bin/python3
+"""
+Script that takes in the name of a state as an argument and lists all cities
+of that state, using the database hbtn_0e_0_usa.
+"""
+import MySQLdb
+import sys
+
+if __name__ == "__main__":
+    if len(sys.argv) == 5:
+        username = sys.argv[1]
+        password = sys.argv[2]
+        db_name = sys.argv[3]
+        state_name = sys.argv[4]
+
+        db = MySQLdb.connect(
+            host="localhost",
+            port=3306,
+            user=username,
+            passwd=password,
+            db=db_name
+        )
+
+        cursor = db.cursor()
+        query = ("SELECT cities.name FROM cities "
+                 "JOIN states ON cities.state_id = states.id "
+                 "WHERE BINARY states.name = %s "
+                 "ORDER BY cities.id ASC")
+        cursor.execute(query, (state_name,))
+        rows = cursor.fetchall()
+
+        cities = [row[0] for row in rows]
+        print(", ".join(cities))
+
+        cursor.close()
+        db.close()
